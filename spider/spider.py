c5ad36f0dbf32585b0220dbff330cfa5febee639
choway <354988859@qq.com>
2019-03-16 16:06:14 +0800

    crawl product list.

--- spider/spider.py
+++ spider/spider.py
@@ -5,15 +5,24 @@ from lxml import etree
 from session import SessionHelper
 
 
-url_1 = 'https://www.champssports.com'
+url = 'https://www.footaction.com/category/mens/shoes/new.html'
 
 
 def crawl():
     session_helper = SessionHelper()
     session = session_helper.get_session()
-    session.get(url_1)
-    session_helper.print_cookies()
+    r = session.get(url)
 
+    session_helper.print_cookies()
+#   print(r.text)
+
+    html = etree.HTML(r.text)
+    product_list = html.xpath('//div[@class="SearchResults"]/ul[@class="row row-2cols--xs row-3cols--sm gutter"]/li[@class="product-container col"]')
+    for product in product_list:
+        href = product.xpath('./div[@class="c-product-card"]/a/@href')[0]
+        name = product.xpath('./div[@class="c-product-card"]/a/span[@class="product-name"]/span/span[@class="primary"]/text()')[0]
+        price = product.xpath('./div[@class="c-product-card"]/div[@class="c-product-price"]//span[@aria-hidden="true"]/text()')[0]
+        print(href, name, price)
 
 
 if __name__ == "__main__":
